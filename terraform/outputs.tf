8c63b9b67eeb74eaffe97f44118d6c60386aacac
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2026-05-16 00:03:48 +0530

    new

--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "s3_artifacts_bucket" { value = aws_s3_bucket.artifacts.id }
-
-output "ecr_repository" { value = aws_ecr_repository.app.repository_url }
-
-output "alb_dns" { value = aws_lb.app.dns_name }
