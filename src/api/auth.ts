b3670bd86d3345ff3ce3274d7590f2563c07cb5a
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2026-07-20 18:02:16 +0530

    new

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "http://localhost:3000/api/auth";
 
 export const registerUser = async (userData: { username: string; email: string; password: string }) => {
   return await axios.post(`${API_URL}/register`, userData);
